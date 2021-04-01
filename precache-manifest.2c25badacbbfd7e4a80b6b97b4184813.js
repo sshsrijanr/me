bf982bbdb4db538fadc469aaeb7bed4e2174f003
srijan-ramavat <srijan.ramavat@gmail.com>
2021-04-02 00:53:30 +0530

    Updates

---
+++ precache-manifest.2c25badacbbfd7e4a80b6b97b4184813.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00fae56b11a63f712be38bcd530810c4",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "feccb498bf96c502b095",
+    "url": "/me/static/css/main.fedf132f.chunk.css"
+  },
+  {
+    "revision": "753022e66805f573b7b9",
+    "url": "/me/static/js/2.6e62842d.chunk.js"
+  },
+  {
+    "revision": "bc24e72e8748236072fbe927f9b37ae6",
+    "url": "/me/static/js/2.6e62842d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feccb498bf96c502b095",
+    "url": "/me/static/js/main.8b92b32b.chunk.js"
+  },
+  {
+    "revision": "3451ae733a78c8f2a0c5",
+    "url": "/me/static/js/runtime-main.8c69244e.js"
+  },
+  {
+    "revision": "38b460c093ec45eaae9daa2561e53d45",
+    "url": "/me/static/media/Resume_Srijan.38b460c0.pdf"
+  },
+  {
+    "revision": "fdab07bde44b76a5ae67a51795ce6250",
+    "url": "/me/static/media/avatar.fdab07bd.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/me/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "1d54217f9df2a44d500dde7c1808de65",
+    "url": "/me/static/media/bookit.1d54217f.png"
+  },
+  {
+    "revision": "2d6ac55edcb07a8bc47c54d2d7b7fa4f",
+    "url": "/me/static/media/cleanItUp.2d6ac55e.png"
+  },
+  {
+    "revision": "d4a6432dc00827c33dabb8ff48ed6ad2",
+    "url": "/me/static/media/dj_rest.d4a6432d.jpeg"
+  },
+  {
+    "revision": "42a78bd737e4bc7b9ea524f3594ae4ea",
+    "url": "/me/static/media/fastlane.42a78bd7.jpeg"
+  },
+  {
+    "revision": "6de72bbdca9c178910a3363e4af41930",
+    "url": "/me/static/media/jenkins.6de72bbd.jpeg"
+  },
+  {
+    "revision": "6d7b2ec232e4091f8408dfd25ec9504d",
+    "url": "/me/static/media/logo.6d7b2ec2.png"
+  },
+  {
+    "revision": "52745d75de32287d9cda7f4bd95ca5d8",
+    "url": "/me/static/media/ocular_profile.52745d75.png"
+  },
+  {
+    "revision": "e06637aae6547ac0465a7ee5189d4bc5",
+    "url": "/me/static/media/postgres.e06637aa.jpeg"
+  },
+  {
+    "revision": "57d16bad10fdb24cbfd76affd7fba371",
+    "url": "/me/static/media/react.57d16bad.jpg"
+  },
+  {
+    "revision": "1456103cc2e43e9370978c28f1cd2230",
+    "url": "/me/static/media/twilio-sendgrid.1456103c.png"
+  }
+]);
